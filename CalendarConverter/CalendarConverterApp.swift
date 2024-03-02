1a763ec1d07699b68652fd090f4511717b1a8ee6
Adam Tokarski <a.tokarski126@gmail.com>
2024-03-02 23:03:09 +0100

    Initial Commit

---
+++ CalendarConverter/CalendarConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarConverterApp.swift
+//  CalendarConverter
+//
+//  Created by Adam Tokarski on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
