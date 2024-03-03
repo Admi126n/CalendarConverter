7f0159591f764b7e15b4e3b7e38f3ec727f210b2
Adam Tokarski <a.tokarski126@gmail.com>
2024-03-03 17:37:17 +0100

    Add calendar picker
    Add calendar picker view.
    Update project settings.

---
+++ CalendarConverter/View/CalendarPicker.swift
@@ -0,0 +1,50 @@
+//
+//  CalendarPicker.swift
+//  CalendarConverter
+//
+//  Created by Adam Tokarski on 03/03/2024.
+//
+
+import EventKit
+import SwiftUI
+
+struct CalendarPicker: View {
+	@Binding var calendar: EKCalendar?
+	
+	let pickerTitle: String
+	let content: [EKCalendar]
+	
+	var body: some View {
+		Form {
+			ForEach(content, id: \.self) { element in
+				HStack {
+					Circle()
+						.frame(width: 10)
+						.foregroundStyle(Color(cgColor: element.cgColor))
+					
+					Text(element.title)
+					
+					Spacer()
+					
+					if element == calendar {
+						Image(systemName: "checkmark")
+							.foregroundStyle(.green)
+					}
+				}
+				.contentShape(.rect)
+				.onTapGesture {
+					withAnimation {
+						calendar = element
+					}
+				}
+			}
+		}
+	}
+	
+	init(title pickerTitle: String, calendar: Binding<EKCalendar?>, content: [EKCalendar]) {
+		self.pickerTitle = pickerTitle
+		self.content = content
+		
+		self._calendar = calendar
+	}
+}
