bce4c4051b04d867cd6c76b834a0f7bbea97c2cc
Adam Tokarski <a.tokarski126@gmail.com>
2024-03-27 21:11:46 +0100

    Add colorPicker for choosing new calendar color

--- CalendarConverter/View/CreateNewCalendarView.swift
+++ CalendarConverter/View/CreateNewCalendarView.swift
@@ -13,18 +13,29 @@ struct CreateNewCalendarView: View {
 	@Environment(\.dismiss) var dismiss
 	@EnvironmentObject var connector: CalendarConnector
 	@FocusState private var focused: Bool
+	@State private var calendarColor = CGColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
 	@State private var calendarTitle = ""
 	
 	let onCalendarCreation: (EKCalendar) -> ()
 	
+	private var disableButton: Bool {
+		calendarTitle.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+	}
+	
+	private var trimmedCalendarTitle: String {
+		calendarTitle.trimmingCharacters(in: .whitespacesAndNewlines)
+	}
+	
     var body: some View {
 		Form {
 			Section {
-				TextField("New calendar title", text: $calendarTitle)
+				TextField("Calendar title", text: $calendarTitle)
 					.focused($focused)
 				
+				ColorPicker("Calendar color", selection: $calendarColor, supportsOpacity: false)
+				
 				Button {
-					if let newCalendar = connector.createCalendar(with: calendarTitle) {
+					if let newCalendar = connector.createCalendar(with: calendarTitle, color: calendarColor) {
 						onCalendarCreation(newCalendar)
 						dismiss()
 					}
@@ -33,6 +44,7 @@ struct CreateNewCalendarView: View {
 						.frame(maxWidth: .infinity)
 				}
 				.buttonStyle(.borderedProminent)
+				.disabled(disableButton)
 			}
 			
 			Button {
